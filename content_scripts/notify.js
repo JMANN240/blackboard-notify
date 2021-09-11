30963da70af0b6ddd2452d9db337eb3d7d5e5591
JT Raber <jmann240raber@gmail.com>
2021-09-11 19:27:06 -0400

    styles and functionality

--- content_scripts/notify.js
+++ content_scripts/notify.js
@@ -1,44 +1,43 @@
 console.log("Hello from notify!")
 
-var participants;
-var url;
+var participants, url, listening;
+
+browser.storage.local.get("checked").then((obj) => {
+    listening = obj.checked;
+});
+
+browser.storage.local.get("url").then((obj) => {
+    url = obj.url;
+});
 
 let participant_observer = new MutationObserver((mutations, observer) => {
     previous_participants = participants;
     participants = parseInt(mutations[0].target.textContent.split(' ')[0]);
-    if (listening) {
-        if (previous_participants < participants) {
-            console.log("SOMEONE HAS JOINED");
+        if (listening && previous_participants < participants) {
             browser.storage.local.get("url").then((obj) => {
-                console.log(obj.url);
                 var req = new XMLHttpRequest();
                 req.open("GET", obj.url);
                 req.send();
-                console.log("sent req");
             });
-        } else if (previous_participants > participants) {
-            console.log("SOMEONE HAS LEFT");
         }
-    }
 });
 
 let page_observer = new MutationObserver((mutations, observer) => {
     let participants_header = document.getElementById("participants-header");
     if (participants_header != null) {
+        participants = parseInt(participants_header.children[0].innerHTML.split(' ')[0]);
         participant_observer.observe(participants_header.children[0], {childList: true});
     } else {
         participant_observer.disconnect();
     }
-    
 });
 
-page_observer.observe(document.body, {subtree: true, childList: true});
-
 browser.runtime.onMessage.addListener((message) => {
-    if (message.message == "listen") {
-        
+    if (message.message == "check") {
+        listening = message.checked;
     } else if (message.message == "url") {
-        console.log(message.url);
         url = message.url;
     }
 });
+
+page_observer.observe(document.body, {subtree: true, childList: true});
--- popup/settings.js
+++ popup/settings.js
@@ -1,6 +1,18 @@
 var checkbox = document.getElementById("toggle-checkbox");
 var url_input = document.getElementById("url-input");
 
+
+
+browser.storage.local.get("checked").then((obj) => {
+    checkbox.checked = obj.checked;
+});
+
+browser.storage.local.get("url").then((obj) => {
+    url_input.value = obj.url;
+});
+
+
+
 checkbox.addEventListener("click", (e) => {
     browser.storage.local.set({checked: checkbox.checked});
     browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
@@ -11,14 +23,12 @@ checkbox.addEventListener("click", (e) => {
     });
 });
 
-browser.storage.local.get("checked").then((obj) => {
-    checkbox.checked = obj.checked;
-});
-
-browser.storage.local.get("url").then((obj) => {
-    url_input.value = obj.url;
-});
-
 url_input.addEventListener("change", (e) => {
     browser.storage.local.set({url: url_input.value});
+    browser.tabs.query({active: true, currentWindow: true}).then((tabs) => {
+        browser.tabs.sendMessage(tabs[0].id, {
+            message: "url",
+            url: url_input.value
+        });
+    });
 });
