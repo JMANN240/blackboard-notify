303f6e5dd08c0aae0701ce9fedc914d4112713ef
JT Raber <jmann240raber@gmail.com>
2021-10-19 22:43:55 -0400

    fixed the bug where the whole thing stopped working

--- content_scripts/notify.js
+++ content_scripts/notify.js
@@ -1,6 +1,6 @@
 console.log("Hello from notify!")
 
-var participants, url, listening;
+let participants, url, listening;
 
 browser.storage.local.get("checked").then((obj) => {
     listening = obj.checked;
@@ -10,23 +10,32 @@ browser.storage.local.get("url").then((obj) => {
     url = obj.url;
 });
 
-var participants_tab_xpath = "//*[@id='panel-control-participants']";
+let participants_tab_xpath = "//*[@id='panel-control-participants']";
 
-var participants_tab_node = document.evaluate(participants_tab_xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+let participants_tab_node;
+
+let findParticipantsTabNode = () => {
+    participants_tab_node = document.evaluate(participants_tab_xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (participants_tab_node == null) {
+        setTimeout(findParticipantsTabNode, 100);
+    } else {
+        participant_observer.observe(participants_tab_node, {attributeFilter: ["aria-label"], attributeOldValue: true});
+    }
+}
+
+setTimeout(findParticipantsTabNode, 1000);
 
 let participant_observer = new MutationObserver((mutations, observer) => {
-    var old_value = parseInt(mutations[0].oldValue.split(' ')[1]);
-    var new_value = parseInt(mutations[0].target.getAttribute("aria-label").split(' ')[1]);
+    let old_value = parseInt(mutations[0].oldValue.split(' ')[1]);
+    let new_value = parseInt(mutations[0].target.getAttribute("aria-label").split(' ')[1]);
     if (listening && new_value > old_value) {
         browser.storage.local.get("url").then((obj) => {
-            var req = new XMLHttpRequest();
+            let req = new XMLHttpRequest();
             req.open("GET", obj.url);
             req.send();
         });
     }
 });
-
-participant_observer.observe(participants_tab_node, {attributeFilter: ["aria-label"], attributeOldValue: true});
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.message == "check") {
